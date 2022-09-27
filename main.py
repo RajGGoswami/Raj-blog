c72bdd5d7f14f43a1c6be00bc5640c53c506a330
RajGGoswami <109051791+RajGGoswami@users.noreply.github.com>
2022-09-27 19:31:30 +1300

    Update main.py

--- main.py
+++ main.py
@@ -221,6 +221,5 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
